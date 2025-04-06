3baf1e0de325b32f516b78d55e553267e0a76bc3
hdsjejgh <140638415+hdsjejgh@users.noreply.github.com>
2025-04-06 08:53:45 -0400

    Update mp_functions.py

--- mp_functions.py
+++ mp_functions.py
@@ -17,6 +17,6 @@ def mp_draw(img,results):
 
 def extract(results):
     pose = np.array([[res.x, res.y, res.x, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros((132,))
-    rh = np.array([[res.x, res.y, res.x] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros((84,))
-    lh = np.array([[res.x, res.y, res.x] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros((84,))
-    return np.concatenate([pose, lh, rh])
+    rh = np.array([[res.x, res.y, res.x] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros((63,))
+    lh = np.array([[res.x, res.y, res.x] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros((63,))
+    return np.concatenate([lh, rh])
